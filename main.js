ccf49f81a99a5e4fd1332f55dc0b3f4d6d96b428
MarkBoomer <mark.boomer@9habu.com>
2020-04-23 00:56:10 +0100

    Add Vue components to replace the links section
    Added 4 vue instance and 4 vue components to replace the latest, most
    read, coming soon articles and related sites links at the end of each page.
    This means the data can be updated in main.js just once and each page
    will be updated automatically

--- main.js
+++ main.js
@@ -22,6 +22,56 @@ Vue.component("missing-person", {
     `
 })
 
+//<!-- ********************************************************************************************* -->
+//<!-- this is the latest articles VUE componment that we bind to the latPopMostComOth VUE Instance  -->
+//<!-- ********************************************************************************************* -->
+Vue.component(‘latest’, {
+    props: [
+        articleList: {
+                       type: Array,
+                       required: true
+                    }
+        ],
+    template: '<li><a href="{{ article-location }}{{ article-name }}">{{ article-title }}</a></li>'
+})
+
+//<!-- ********************************************************************************************* -->
+//<!-- this is the most read articles VUE componment that we bind to the latPopMostComOth VUE Instance  -->
+//<!-- ********************************************************************************************* -->
+Vue.component(‘most-read’, {
+    props: [
+        articleList: {
+                       type: Array,
+                       required: true
+                    }
+        ],
+    template: '<li><a href="{{ article-location }}{{ article-name }}">{{ article-title }}</a></li>'
+})
+//<!-- ********************************************************************************************* -->
+//<!-- this is the coming soon articles VUE componment that we bind to the latPopMostComOth VUE Instance  -->
+//<!-- ********************************************************************************************* -->
+Vue.component(‘coming-soon’, {
+    props: [
+        articleList: {
+                       type: Array,
+                       required: true
+                    }
+        ],
+    template: '<li>{{ article }}</li>'
+})
+//<!-- ********************************************************************************************* -->
+//<!-- this is the related sites VUE componment that we bind to the latPopMostComOth VUE Instance  -->
+//<!-- ********************************************************************************************* -->
+Vue.component(‘related-sites’, {
+    props: [
+        siteList: {
+                    type: Array,
+                    required: true
+                  }
+        ],
+    template: '<li><a href="{{ site-url }}">{{ site-title }}</a></li>'
+})
+
 //<!-- ************************************************************************************************ -->
 //<!-- vue instance attached to the <UL> html element - contains the data for each person               -->
 //<!-- ************************************************************************************************ -->
@@ -55,5 +105,64 @@ var missingPassengers = new Vue({
                 personsDesc:  "Carmel was a 47 year Maltese national and the pilot of flight 9H-ABU" 
             }
         ]
+    }
+})
+//<!-- *********************************************************************************************************** -->
+//<!-- vue instance attached to the DIV html element "latest-popular-soon" containing the data for each categories -->
+//<!-- *********************************************************************************************************** -->
+
+var latMosComRel = new Vue({
+    el: ‘#latest-popular-soon’,
+    data: {
+        latestArticlesTitle: "Latest Articles",
+        latestArticles: [
+                            {   article-name     : "freedom-of-info-request-15112015.html",
+                                article-title    : "Freedom Of Information Request AAIB"
+                                article-location : "articles/"
+                            },
+                            {   article-name     : "freedom-of-info-response-14122015.html",
+                                article-title    : "FOI Response from AAIB"
+                                article-location : "articles/"
+                            },
+                            {   article-name     : "tv-documentary-first-shown-on-malta-television-26112011.html",
+                                article-title    : "Updated Video Player for Malta TV Documentary"
+                                article-location : "articles/"
+                            }
+                        ],
+        mostReadArticlesTitle: "Most Read",
+        mostReadArticles: [
+                            {   article-name     : "banbridge-chronicle-30112011.html",
+                                article-title    : "QUEST FOR TRUTH, Banbridge Chronicle"
+                                article-location : "articles/"
+                            },
+                            {   article-name     : "death-in-the-mediterranean.html",
+                                article-title    : "An Investigation By Don Mullan"
+                                article-location : "./"
+                            },
+                            {   article-name     : "maltese-board-of-inquiry-report.html",
+                                article-title    : "Maltese Board of Inquiry Report"
+                                article-location : "./"
+                            },
+                            {   article-name  : "malta-tv-documentary.html",
+                                article-title : "Malta TV Documentary"
+                                article-location : "./"
+                            }
+                        ],
+        comingSoonArticlesTitle: "Coming Soon",
+        comingSoonArticles: [   "Updated responsive website suitable for mobile and tablets",
+                                "Improved Document Archive Search"
+                            ],
+        relatedSitesTitle: "Related Web Sites",
+        relatedSites: [
+                        {   site-url   : "www.flightlinemalta.com",
+                            site-title : "Flightline GA Malta"
+                        },
+                        {   site-url   : "www.pprune.org",
+                            site-title : "Professional Pilots Rumour Network"
+                        },
+                        {   site-url   : "https://jetphotos.com/photo/71054",
+                            site-title : "Jet Photos - 9H-ABU"
+                        }
+                    ]
     }
 })
