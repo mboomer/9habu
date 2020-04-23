64f5bdb3fd0832a828df6bf17e258c3dccc10b88
MarkBoomer <mark.boomer@9habu.com>
2020-04-23 23:06:26 +0100

    Fix the issue with the location + filename
    Fixed the problem of building the full URL from the
    file location and the filename
    Some articles are in articles/ some are in root ./

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ Vue.component("latest", {
                        required: true
                     }
     },
-    template: '<li><a :href="latestarticle.latestlocation">{{ latestarticle.latesttitle }}</a></li>'
+    template: '<li><a :href="latestarticle.latestlocation + latestarticle.latestname">{{ latestarticle.latesttitle }}</a></li>'
 })
 //<!-- ********************************************************************************************* -->
 //<!-- this is the most read articles VUE componment that we bind to the latPopMostComOth VUE Instance  -->
@@ -43,7 +43,7 @@ Vue.component("most-read", {
                        required: true
                     }
     },
-    template: '<li><a :href="mostreadarticle.mostreadlocation">{{ mostreadarticle.mostreadtitle }}</a></li>'
+    template: '<li><a :href="mostreadarticle.mostreadlocation + mostreadarticle.mostreadname">{{ mostreadarticle.mostreadtitle }}</a></li>'
 })
 //<!-- ********************************************************************************************* -->
 //<!-- this is the coming soon articles VUE componment that we bind to the latPopMostComOth VUE Instance  -->
@@ -116,44 +116,40 @@ var latMosComRel = new Vue ({
         latestArticles: [
                             {   latestname     : "freedom-of-info-request-15112015.html",
                                 latesttitle    : "Freedom Of Information Request AAIB",
-                                latestlocation : "articles/freedom-of-info-request-15112015.html"
+                                latestlocation : "articles/"
                             },
                             {   latestname     : "freedom-of-info-response-14122015.html",
                                 latesttitle    : "FOI Response from AAIB",
-                                latestlocation : "articles/freedom-of-info-response-14122015.html"
+                                latestlocation : "articles/"
                             },
                             {   latestname     : "tv-documentary-first-shown-on-malta-television-26112011.html",
                                 latesttitle    : "Updated Video Player for Malta TV Documentary",
-                                latestlocation : "articles/tv-documentary-first-shown-on-malta-television-26112011.html"
+                                latestlocation : "articles/"
                             }
                         ],
         mostReadArticlesTitle: "Most Read",
         mostReadArticles: [
                             {   mostreadname     : "banbridge-chronicle-30112011.html",
                                 mostreadtitle    : "QUEST FOR TRUTH, Banbridge Chronicle",
-                                mostreadlocation : "articles/banbridge-chronicle-30112011.html"
+                                mostreadlocation : "articles/"
                             },
                             {   mostreadname     : "death-in-the-mediterranean.html",
                                 mostreadtitle    : "An Investigation By Don Mullan",
-                                mostreadlocation : "./death-in-the-mediterranean.html"
+                                mostreadlocation : "./"
                             },
                             {   mostreadname     : "maltese-board-of-inquiry-report.html",
                                 mostreadtitle    : "Maltese Board of Inquiry Report",
-                                mostreadlocation : "./maltese-board-of-inquiry-report.html"
+                                mostreadlocation : "./"
                             },
                             {   mostreadname     : "malta-tv-documentary.html",
                                 mostreadtitle    : "Malta TV Documentary",
-                                mostreadlocation : "./malta-tv-documentary.html"
+                                mostreadlocation : "./"
                             }
                         ],
         comingSoonArticlesTitle: "Coming Soon",
         comingSoonArticles: [ 
-                                {   
-                                    comingsoonname   : "Updated responsive website suitable for mobile and tablets"
-                                },
-                                {   
-                                    comingsoonname   : "Improved Document Archive Search"
-                                }
+                                {comingsoonname   : "Updated responsive website suitable for mobile and tablets"},
+                                {comingsoonname   : "Improved Document Archive Search"}
                             ],
         relatedSitesTitle: "Related Web Sites",
         relatedSites: [
@@ -167,5 +163,5 @@ var latMosComRel = new Vue ({
                             sitetitle : "Jet Photos - 9H-ABU"
                         }
                     ]
-    }
+    },
 })
