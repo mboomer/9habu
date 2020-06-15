27e941a0b5d57dcc6cb9462fcdd896d69ef6c827
MarkBoomer <mark.boomer@9habu.com>
2020-06-15 10:38:28 +0100

    Modify article to correct image size

--- search.php
+++ search.php
@@ -1,7 +1,7 @@
 <?php
-    if ($_GET["q"] == "Search...") {
-        header("Location: search.php");
-    }
+//    if ($_GET["q"] == "Search...") {
+//        header("Location: search.php");
+//    }
 
     $servername = "mysql.9habu.com";
     $username   = "9habu_db";
@@ -52,118 +52,156 @@
     <body>         
         <div id="filter-container">
             
-            <div class="drop-down">
-                <div class="category-container">
-                    <label>Category : </label>
-                    <select id="category" name="category">
-                    <option value="">Select Category</option>
-                    <?php 
-                        $sqlCatg = "SELECT DISTINCT category FROM articles ORDER BY category ASC";
-                        $rsCatg  = mysqli_query($conn, $sqlCatg);
+            <form id="search-form" method="GET" action="search.php" >
 
-                        while($rows = mysqli_fetch_assoc($rsCatg)) {
-                            echo '<option value="'.$rows['category'].'">'.$rows['category'].'</option>';
-                        }
-                    ?>
-                    </select>
-                <img src="" id="loader">
+                <div class="drop-down">
+                    <fieldset id="category">
+                        <legend>Category</legend>
+                        <select id="category" name="category">
+                        <option value="">Select...</option>
+                        <?php 
+                            $sqlCatg = "SELECT DISTINCT category FROM articles ORDER BY category ASC";
+                            $rsCatg  = mysqli_query($conn, $sqlCatg);
+
+                            while($rows = mysqli_fetch_assoc($rsCatg)) {
+                                echo '<option value="'.$rows['category'].'">'.$rows['category'].'</option>';
+                            }
+                        ?>
+                        </select>
+                    </fieldset>
                 </div>
-            </div>
 
-            <div class="drop-down">
-                <div class="author-container">
-                    <label>Author : </label>
-                    <select id="author" name="author">
-                    <option value="">Select Author</option>
-                    <?php 
-                        $sqlAuth = "SELECT DISTINCT author FROM articles ORDER BY author ASC";
-                        $rsAuth  = mysqli_query($conn, $sqlAuth);
+                <div class="drop-down">
+                    <fieldset id="author">
+                        <legend>Author : </legend>
+                        <select id="author" name="author">
+                        <option value="">Select...</option>
+                        <?php 
+                            $sqlAuth = "SELECT DISTINCT author FROM articles ORDER BY author ASC";
+                            $rsAuth  = mysqli_query($conn, $sqlAuth);
 
-                        while($rows = mysqli_fetch_assoc($rsAuth)) {
-                            echo '<option value="'.$rows['author'].'">'.$rows['author'].'</option>';
-                        }
-                    ?>
+                            while($rows = mysqli_fetch_assoc($rsAuth)) {
+                                echo '<option value="'.$rows['author'].'">'.$rows['author'].'</option>';
+                            }
+                        ?>
                     </select>
-<!--                <img src="" id="loader">-->
+                    </fieldset>
                 </div>
-            </div>
 
-            <div class="drop-down">
-                <div class="year-container">
-                    <label>Year : </label>
-                    <select id="year" name="year">
-                    <option value="">Select Year</option>
-                    <?php 
-                        $sqlYear = "SELECT DISTINCT year     FROM articles ORDER BY year ASC";
-                        $rsYear  = mysqli_query($conn, $sqlYear);
+                <div class="drop-down">
+                    <fieldset id="year">
+                        <legend>Year</legend>
+                        <select id="year" name="year">
+                        <option value="">Select...</option>
+                        <?php 
+                            $sqlYear = "SELECT DISTINCT year FROM articles ORDER BY year ASC";
+                            $rsYear  = mysqli_query($conn, $sqlYear);
 
-                        while($rows = mysqli_fetch_assoc($rsYear)) {
-                            echo '<option value="'.$rows['year'].'">'.$rows['year'].'</option>';
-                        }
-                    ?>
-                    </select>
-                <img src="" id="loader">
+                            while($rows = mysqli_fetch_assoc($rsYear)) {
+                                echo '<option value="'.$rows['year'].'">'.$rows['year'].'</option>';
+                            }
+                        ?>
+                        </select>
+                    </fieldset>
                 </div>
-            </div>
 
-            <div class="drop-down">
-                <div class="month-container">
-                    <label>Month : </label>
-                    <select id="month" name="month">
-                    <option value="">Select Month</option>
-                    <?php 
-                        $sqlMnth = "SELECT DISTINCT month FROM articles ORDER BY year ASC";
-                        $rsMnth  = mysqli_query($conn, $sqlMnth);
+                <div class="drop-down">
+                    <fieldset id="month">
+                        <legend>Month</legend>
+                        <select id="month" name="month">
+                        <option value="">Select...</option>
+                        <?php 
+                            $sqlMnth = "SELECT DISTINCT month FROM articles ORDER BY year ASC";
+                            $rsMnth  = mysqli_query($conn, $sqlMnth);
 
-                        while($rows = mysqli_fetch_assoc($rsMnth)) {
-                            echo '<option value="'.$rows['month'].'">'.$rows['month'].'</option>';
-                        }
-                    ?>
-                    </select>
-                <img src="" id="loader">
+                            while($rows = mysqli_fetch_assoc($rsMnth)) {
+                                echo '<option value="'.$rows['month'].'">'.$rows['month'].'</option>';
+                            }
+                        ?>
+                        </select>
+                    </fieldset>
                 </div>
-            </div>
 
-            <div class="drop-down">
-                <div class="day-container">
-                    <label>Day : </label>
-                    <select id="day" name="day">
-                    <option value="">Select Day</option>
-                    <?php 
-                        $sqlDays = "SELECT DISTINCT day FROM articles ORDER BY day ASC";
-                        $rsDays  = mysqli_query($conn, $sqlDays);
+                <div class="drop-down">
+                    <fieldset id="day">
+                        <legend>Day</legend>
+                        <select id="day" name="day">
+                        <option value="">Select...</option>
+                        <?php 
+                            $sqlDays = "SELECT DISTINCT day FROM articles ORDER BY day ASC";
+                            $rsDays  = mysqli_query($conn, $sqlDays);
 
-                        while($rows = mysqli_fetch_assoc($rsDays)) {
-                            echo '<option value="'.$rows['day'].'">'.$rows['day'].'</option>';
-                        }
-                    ?>
-                    </select>
-                <img src="" id="loader">
+                            while($rows = mysqli_fetch_assoc($rsDays)) {
+                                echo '<option value="'.$rows['day'].'">'.$rows['day'].'</option>';
+                            }
+                        ?>
+                        </select>
+                    </fieldset>
                 </div>
-            </div>
 
-            <div id="search" class="drop-down">
-                <form id="searchForm" method="GET" action="search.php" >
-                    <input id="searchBox" name="q" type="text" placeholder="" value="Search..." maxlength="50" autocomplete="off" onmousedown="active();" onblur="inactive();" /><input id="searchBtn" name="submit" type="submit" value="Go!" />
-                </form>
-
-            </div>
+                <div class="drop-down">
+                    <fieldset id="search">
+                        <input id="searchBox" name="q" type="text" placeholder="Search..." value="" maxlength="50" autocomplete="off" onmousedown="active();" onblur="inactive();" /><input id="searchBtn" name="submit" type="submit" value="Go!" />
+                    </fieldset>
+                </div>
+            </form>
 
         </div>
 
         <?php
-            if (!isset($searchString)) {
-                echo "no search entered";
-            } else {
-                    $queryString = 'SELECT * FROM articles WHERE title LIKE "%' .$searchString . '%" OR description LIKE "%' .$searchString . '%"';
+            
+            $_category = $_GET["category"];
+            $_author   = $_GET["author"];
+            $_year     = $_GET["year"];
+            $_month    = $_GET["month"];
+            $_day      = $_GET["day"];
 
+            if (!isset($searchString)) {
+                echo "<div class='results'>No Search Entered</div>";
+            } else {
+                    $queryString = 'SELECT * FROM articles WHERE (title LIKE "%' .$searchString . '%" OR description LIKE "%' .$searchString . '%")';
+                
+                    if ($_category !== "") {
+                        $queryString = $queryString . ' AND (category = "' . $_category . '")';
+                    }                
+                    if ($_author !== "") {
+                        $queryString = $queryString . ' AND (author = "' . $_author . '")';
+                    }
+                    if ($_year !== "") {
+                        $queryString = $queryString . ' AND (year = "' . $_year . '")';
+                    }
+                    if ($_month !== "") {
+                        $queryString = $queryString . ' AND (month = "' . $_month . '")';
+                    }
+                    if ($_day !== "") {
+                        $queryString = $queryString . ' AND (day = "' . $_day . '")';
+                    }
+                
+                    echo $queryString;
+                    
                     $query = mysqli_query($conn, $queryString);    
                     $num_rows = mysqli_num_rows($query);
 
+                   if ($_category !== "") {
+                        $searchString = $searchString . ' category: ' . $_category;
+                    }                
+                    if ($_author !== "") {
+                        $searchString = $searchString . ' author: ' . $_author;
+                    }
+                    if ($_year !== "") {
+                        $searchString = $searchString . ' year: ' . $_year;
+                    }
+                    if ($_month !== "") {
+                        $searchString = $searchString . ' month: ' . $_month;
+                    }
+                    if ($_day !== "") {
+                        $searchString = $searchString . ' day: ' . $_day;
+                    }
+ 
                     if ($num_rows == 1) {
-                        echo "<p><strong>" . $num_rows . "</strong> result for " . $searchString . "</p>";
+                        echo "<p class='result-header'><strong>" . $num_rows . "</strong> Result For Search &quot;" . $searchString . "&quot;</p>";
                     } else {
-                        echo "<p><strong>" . $num_rows . "</strong> results for " . $searchString . "</p>";
+                        echo "<p class='result-header'><strong>" . $num_rows . "</strong> Results For Search &quot;" . $searchString . "&quot;</p>";
                     }
 
                     while($row = mysqli_fetch_array($query)) {
@@ -177,9 +215,9 @@
                         $author      = $row["author"];
                         $link        = $row["link"];
 
-                        echo "<div id='results'>";
+                        echo "<div class='results'>";
 
-                            echo "<h3>" . $title . "</h3>" . "<p>" . $description . "</p>" . "<p><strong>Category : </strong>" . $category . "<br>" . "<strong>Author : </strong>" . $author . "<br>" . "<strong>Date : </strong>" . $day . "/" . $month . "/" . $year . "<br>" . "<strong>Article : </strong><a href='articles/" . $link . "'>" . $title . "</a>" . "<br>";
+                            echo "<h3>" . $title . "</h3>" . "<p>" . $description . "</p>" . "<p><strong>Category : </strong>" . $category . "<br>" . "<strong>Author : </strong>" . $author . "<br>" . "<strong>Date : </strong>" . $month . " " . $day . " " . $year . "<br>" . "<strong>Article : </strong><a href='articles/" . $link . "'>" . $title . "</a>" . "<br>";
 
                         echo "</div>";
                         }
